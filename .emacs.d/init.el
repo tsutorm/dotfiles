0e16af414b59b25387a6c31f9d2e3f7828ceb404
tsutorm <oonishi.wasborn.1981@gmail.com>
2015-02-17 01:15:08 +0900

    helm追加

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -46,6 +46,44 @@
 (require 'auto-complete)
 (global-auto-complete-mode 1)
 
+
+;;; helm
+(require 'helm)
+(require 'helm-config)
+(require 'helm-gtags)
+;; 標準を置き換え
+(helm-mode +1)
+;;(global-set-key (kbd "C-x b") 'helm-for-files)
+;;(global-set-key (kbd "C-x C-f") 'helm-find-files)
+;; helmショートカット
+(global-set-key (kbd "C-x C-h h") 'helm-mini)
+(global-set-key (kbd "C-x C-h r") 'helm-recentf)
+(global-set-key (kbd "C-x C-h i") 'helm-imenu)
+(global-set-key (kbd "C-x C-h k") 'helm-show-kill-ring)
+;; helmコマンド内のキーバインド
+(define-key helm-map (kbd "C-h") 'delete-backward-char)
+(define-key helm-read-file-map (kbd "C-h") 'delete-backward-char)
+(define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
+(define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
+(define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
+;; helm-gtags-mode
+(add-hook 'helm-gtags-mode-hook
+'(lambda ()
+   ;;入力されたタグの定義元へジャンプ
+   (local-set-key (kbd "M-t") 'helm-gtags-find-tag)
+   ;;入力タグを参照する場所へジャンプ
+   (local-set-key (kbd "M-r") 'helm-gtags-find-rtag)
+   ;;入力したシンボルを参照する場所へジャンプ
+   (local-set-key (kbd "M-s") 'helm-gtags-find-symbol)
+   ;;タグ一覧からタグを選択し, その定義元にジャンプする
+   (local-set-key (kbd "M-l") 'helm-gtags-select)
+   ;;ジャンプ前の場所に戻る
+   (local-set-key (kbd "C-t") 'helm-gtags-pop-stack)))
+(add-hook 'php-mode-hook 'helm-gtags-mode)
+;(add-hook 'ruby-mode-hook 'helm-gtags-mode)
+
+
+
 ;;; prog-modes
 ;; js2-mode
 (autoload 'js2-mode "js2" nil t)
